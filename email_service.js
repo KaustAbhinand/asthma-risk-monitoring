388a71e16a41a1facbdaf5091bdec1b4195c8f45
KaustAbhinand <kaustubhabhinan@gmail.com>
2026-05-09 13:10:13 +0530

    Changes to email sending

--- email_service.js
+++ email_service.js
@@ -1,26 +1,23 @@
-
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 console.log("Email service initialized with user:", process.env.EMAIL_USER);
 
-// SMTP transporter
+// Gmail transporter
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
+
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    tls: { rejectUnauthorized: false },
-    connnectionTimeout: 10000,
+
+    connectionTimeout: 10000,
     greetingTimeout: 10000,
     socketTimeout: 10000
-
 });
 
-// Verify connection (debug)
+// Verify connection
 transporter.verify((err, success) => {
     if (err) {
         console.error("SMTP Error:", err);
@@ -40,19 +37,43 @@ async function sendAlertEmail(to, risk, level) {
     try {
         console.log("Sending email to:", to);
 
-        await transporter.sendMail({
+        const mailOptions = {
             from: `"Asthma Risk Prediction" <${process.env.EMAIL_USER}>`,
             to: to.trim(),
             subject: "High Asthma Risk Alert",
+
+            text: `
+High Risk Detected
+
+Your current asthma risk score is ${risk}
+Risk Level: ${level}
+
+Please open your dashboard to see your precautions.
+            `,
+
             html: `
                 <h2>High Risk Detected</h2>
-                <p>Your current asthma risk score is <b>${risk}</b>.</p>
-                <p>Risk Level: <b>${level}</b></p>
-                <p>Please open your dashboard to see your precautions</p>
-            `
-        });
 
-        console.log("✅ Email sent to:", to);
+                <p>Your current asthma risk score is 
+                <b>${risk}</b>.</p>
+
+                <p>Risk Level: <b>${level}</b></p>
+
+                <p>
+                    Please open your dashboard to see 
+                    your precautions.
+                </p>
+            `
+        };
+
+        console.log("Before sendMail");
+
+        const info = await transporter.sendMail(mailOptions);
+
+        console.log("After sendMail");
+        console.log("✅ Email sent:", info.response);
+
+        return info;
 
     } catch (err) {
         console.error("❌ Email error:", err);
